593a2176b2b03e7ea05a88f6b841db5b165b5d72
Damien Pyles <dpyles@yourlightingbrand.com>
2020-07-07 11:50:03 -0400

    enable supression of logging

--- lib/csv_splitter.rb
+++ lib/csv_splitter.rb
@@ -1,7 +1,7 @@
 require 'csv'
 require 'zaru'
 class CsvSplitter
-  def initialize(csv, out_path: '/tmp', column_to_split_by:, transform_headers: nil)
+  def initialize(csv, out_path: '/tmp', column_to_split_by:, transform_headers: nil, suppress_logging: false)
     @transform_header = transform_headers
     @file = File.read(csv).encode('UTF-8', :invalid => :replace, :undef => :replace, :replace => '')
     @out_path = out_path
@@ -15,7 +15,6 @@ class CsvSplitter
     sorting_index = csv_file.headers.index(normalized_key)
     csv_file.sort_by{|v| v[sorting_index] }.each do |row|
       current = Zaru.sanitize!(row[normalized_key].to_s.gsub(/_/, ' ').strip)
-      
       new_file = "#{@out_path}/#{current}_new.csv"
       if last_file != new_file  
         write_mode = 'w'
@@ -27,7 +26,7 @@ class CsvSplitter
       CSV.open(new_file, write_mode, write_headers: write_mode == 'w', headers: row.headers.map(&transform_block)) do |csv|
         csv << row.to_h.values
       end
-      puts "wrote to file for column #{current}"
+      puts "wrote to file for column #{current}" unless suppress_logging
     end
   end
 end
